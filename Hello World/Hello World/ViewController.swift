96d155c36ff03ebbcae0acc6ad9e16d8b2f89348
panox <panospalos@gmail.com>
2016-01-20 19:57:55 +0000

    IBaction

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func submit(sender: AnyObject) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello Panos!")
